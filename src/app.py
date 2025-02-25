5f3f06966a6514956ca75c45c3e22e35bda05340
lab2077 <lalpizarbr@gmail.com>
2025-02-25 19:20:53 +0000

    Proyecto Intro a SQL - Luis Alpizar

--- src/app.py
+++ src/app.py
@@ -80,11 +80,40 @@ with conn:
         (6, 'Richard', NULL, 'Blum'),
         (7, 'Yuval', 'Noah', 'Harari'),
         (8, 'Paul', NULL, 'Albitz');
+    
+    INSERT OR IGNORE INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES 
+        (1, 'Lean Software Development: An Agile Toolkit', 240, 4.17, '9780320000000', '2003-05-18', 5),
+        (2, 'Facing the Intelligence Explosion', 91, 3.87, NULL, '2013-02-01', 7),
+        (3, 'Scala in Action', 419, 3.74, '9781940000000', '2013-04-10', 1),
+        (4, 'Patterns of Software: Tales from the Software Community', 256, 3.84, '9780200000000', '1996-08-15', 1),
+        (5, 'Anatomy Of LISP', 446, 4.43, '9780070000000', '1978-01-01', 3),
+        (6, 'Computing machinery and intelligence', 24, 4.17, NULL, '2009-03-22', 4),
+        (7, 'XML: Visual QuickStart Guide', 269, 3.66, '9780320000000', '2009-01-01', 5),
+        (8, 'SQL Cookbook', 595, 3.95, '9780600000000', '2005-12-01', 7),
+        (9, 'The Apollo Guidance Computer: Architecture And Operation (Springer Praxis Books / Space Exploration)', 439, 4.29, '9781440000000', '2010-07-01', 6),
+        (10, 'Minds and Computers: An Introduction to the Philosophy of Artificial Intelligence', 222, 3.54, '9780750000000', '2007-02-13', 7);
+    
+    INSERT OR IGNORE INTO book_authors (book_id, author_id) VALUES 
+        (1, 1),
+        (2, 8),
+        (3, 7),
+        (4, 6),
+        (5, 5),
+        (6, 4),
+        (7, 3),
+        (8, 2),
+        (9, 4),
+        (10, 1);         
+   
+
+
     """)
 
 # 4) Use Pandas to read and display a table
-df = pd.read_sql("SELECT * FROM publishers;", conn)
-print(df)
+df_books = pd.read_sql("SELECT * FROM books;", conn)
+
+print("\nLibros:")
+print(df_books)
 
 # 5) Close connection
 conn.close()
